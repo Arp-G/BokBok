1c90fa6456bdd7f704bed8097f6d390dc101cf80
Arpan-Kreeti <arpan.ghoshal@kreeti.com>
2020-08-19 20:28:01 +0530

    avoid current user in searches

--- lib/bok_bok/search.ex
+++ lib/bok_bok/search.ex
@@ -2,29 +2,35 @@ defmodule BokBok.Search do
   import Ecto.Query
   alias BokBok.{Repo, Accounts.User, Accounts.UserProfile}
 
-  def find_users(query) do
+  def find_users(query, current_user) do
     from(u in User,
       left_join: up in UserProfile,
       on: up.user_id == u.id,
       or_where: ilike(u.username, ^("%" <> query <> "%")),
       or_where: ilike(up.name, ^("%" <> query <> "%"))
     )
+    |> ignore_current_user(current_user)
     |> Repo.all()
     |> Repo.preload(:user_profile)
   end
 
-  def find_contacts(phone_nos) do
+  def find_contacts(phone_nos, current_user) do
     from(u in User, where: u.phone_number in ^phone_nos)
+    |> ignore_current_user(current_user)
     |> Repo.all()
     |> Repo.preload(:user_profile)
   end
 
-  def get_random_people() do
+  def get_random_people(current_user) do
     max = Repo.aggregate(User, :count, :id)
     random_offset = :rand.uniform(max) - 1
 
     from(u in User, limit: 10, offset: ^random_offset)
+    |> ignore_current_user(current_user)
     |> Repo.all()
     |> Repo.preload(:user_profile)
   end
+
+  defp ignore_current_user(query, current_user),
+    do: where(query, [user], user.id != ^current_user.id)
 end
--- lib/bok_bok_web/controllers/search_controller.ex
+++ lib/bok_bok_web/controllers/search_controller.ex
@@ -3,12 +3,14 @@ defmodule BokBokWeb.SearchController do
   action_fallback BokBokWeb.FallbackController
   alias BokBok.Search
 
-  def search(conn, %{"query" => query}) do
-    search_results = Search.find_users(query)
+  def search(%{assigns: %{current_user: current_user}} = conn, %{"query" => query}) do
+    search_results = Search.find_users(query, current_user)
     render(conn, "show.json", searches: search_results)
   end
 
-  def search_contacts(conn, %{"phone_nos" => phone_nos}) do
+  def search_contacts(%{assigns: %{current_user: current_user}} = conn, %{
+        "phone_nos" => phone_nos
+      }) do
     phone_nos =
       phone_nos
       |> Enum.reject(&is_nil/1)
@@ -16,12 +18,12 @@ defmodule BokBokWeb.SearchController do
         ph |> String.replace(["(", ")", "-", " ", "+"], "")
       end)
 
-    search_results = Search.find_contacts(phone_nos)
+    search_results = Search.find_contacts(phone_nos, current_user)
     render(conn, "show.json", searches: search_results)
   end
 
-  def random_users(conn, _) do
-    search_results = Search.get_random_people()
+  def random_users(%{assigns: %{current_user: current_user}} = conn, _) do
+    search_results = Search.get_random_people(current_user)
     render(conn, "show.json", searches: search_results)
   end
 end
