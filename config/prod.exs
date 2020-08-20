14051461e399f19d7a7161d4ab71e43509338e5f
Arpan-Kreeti <arpan.ghoshal@kreeti.com>
2020-08-20 20:48:17 +0530

    update socket url

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :bok_bok, BokBokWeb.Endpoint,
 http: [port: {:system, "PORT"}],
-url: [scheme: "https", host: "gentle-springs-13724.herokuapp.com", port: 443],
+url: [scheme: "https", host: "bok-bok.herokuapp.com", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
 cache_static_manifest: "priv/static/cache_manifest.json"
 
