9aa3bc7242e4b59a2355445aab7b7b016ba851b0
Arpan-Kreeti <arpan.ghoshal@kreeti.com>
2020-08-20 20:37:15 +0530

    update api url

--- mapp/BokBok/src/helpers/api.js
+++ mapp/BokBok/src/helpers/api.js
@@ -1,3 +1,3 @@
-export const SITE_URL = 'https://gentle-springs-13724.herokuapp.com';
+export const SITE_URL = 'https://bok-bok.herokuapp.com/';
 export const API = `${SITE_URL}/api`;
 export const WEBSOCKET_API = `${SITE_URL.replace('https', 'ws')}/socket`;
